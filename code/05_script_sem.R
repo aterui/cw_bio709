33bacec67fd50abaa93ae1b0dea5306ecaef1b83
Akira Terui <hanabi0111@gmail.com>
2026-02-26 16:18:03 -0500

    📝 sem

--- code/05_script_sem.R
+++ code/05_script_sem.R
@@ -3,6 +3,82 @@
 
 # in-class ----------------------------------------------------------------
 
+pacman::p_load(tidyverse,
+               GGally,
+               vegan,
+               lavaan,
+               lavaanPlot)
+
+# Specify the URL of the raw CSV file on GitHub
+url <- "https://raw.githubusercontent.com/aterui/biostats/master/data_raw/data_foodweb.csv"
+
+# Read the CSV file into a tibble
+(df_fw <- read_csv(url))
+
+# visualize data
+df_fw %>% 
+  select(-plot_id) %>% 
+  ggpairs(
+    progress = FALSE
+  ) +
+  theme_bw()
+
+# write a path diagram
+m1 <- '
+  mass_herbiv ~ mass_plant + cv_h_plant
+  mass_pred ~ mass_herbiv
+'
+
+(fit1 <- sem(model = m1,
+             data = df_fw))
+
+summary(fit1, standardize = TRUE)
+
+lavaanPlot(model = fit1, coefs = TRUE, stand = TRUE)
+
+# model comparison
+
+m2 <- '
+  mass_herbiv ~ mass_plant + cv_h_plant
+  mass_pred ~ mass_herbiv + cv_h_plant
+'
+
+fit2 <- sem(model = m2,
+            data = df_fw)
+
+# model comparison with anova() i.e., likelihood ratio test
+anova(fit1, fit2)
+
+# structural equation model
+url <- "https://raw.githubusercontent.com/aterui/biostats/master/data_raw/data_herbivory.csv"
+
+(df_herbv <- read_csv(url))
+
+# visualize
+df_herbv %>% 
+  ggpairs(
+    progress = FALSE,
+    columns = c("soil_n",
+                "per_lignin",
+                "sla",
+                "cn_ratio")
+  ) +
+  theme_bw()
+  
+m_sem <- '
+  # latent variable
+  palatability =~ sla + per_lignin + cn_ratio
+  
+  # regression
+  palatability ~ soil_n
+  herbivory ~ palatability
+'
+
+(fit_sem <- sem(model = m_sem,
+                data = df_herbv))
+
+summary(fit_sem, standardize = TRUE)
+
 # lab ---------------------------------------------------------------------
 
 # ============================================================
@@ -18,11 +94,10 @@ data("keeley")
 # ------------------------------------------------------------
 # Column descriptions:
 # elev  : Elevation of the site
-# slope : Slope steepness
-# aspect: Slope aspect (orientation)
-# heat  : Heat load index (a function of slope and aspect)
+# abiotic : Abiotic environmental factors (integrated variable indicating the suitability of the habitat)
+# age   : Stand age
+# hetero  : Environmental heterogeneity
 # firesev: Fire severity
-# age   : Time since last fire
 # cover : Vegetation cover
 # rich  : Plant species richness
 # ------------------------------------------------------------
@@ -34,13 +109,48 @@ data("keeley")
 # 1. For the variables depicted in Figure 22.1, draw a figure
 #    showing the covariance between variables.
 
+df_keeley <- as_tibble(keeley)
+
+df_keeley %>% 
+  select(-elev) %>% 
+  ggpairs(progress = F) +
+  theme_bw()
+
 # 2. Following Figure 22.1, develop a path model using the
 #    same variables and relationships. Examine if this model
 #    captures the data structure using a Chi-Square test.
 
+m <- '
+  age ~ distance
+  hetero ~ distance
+  abiotic ~ distance
+  firesev ~ age
+  cover ~ firesev
+  rich ~ abiotic + hetero + cover
+'
+
+fit <- sem(model = m,
+           data = df_keeley)
+
+summary(fit, standardize = TRUE)
+
 # 3. Develop an alternative path model that you consider more
 #    appropriate based on theory or observed data patterns.
+
+m_rev <- '
+  age ~ distance
+  hetero ~ distance
+  abiotic ~ distance
+  firesev ~ age
+  cover ~ firesev + age
+  rich ~ abiotic + hetero + cover + firesev
+'
+
+(fit_rev <- sem(model = m_rev,
+                data = df_keeley))
 
 # 4. Compare the performance of the published model (Figure 22.1)
 #    and your alternative model.
 #    - Consider fit indices, path coefficients, and interpretability.
+
+anova(fit, fit_rev)
