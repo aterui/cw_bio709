3a14d43b20af4f7cd0e640344c6940cb4a74674b
Akira Terui <hanabi0111@gmail.com>
2026-03-19 15:11:56 -0400

    📝 ts 1 chunk

--- code/07_script_ts.R
+++ code/07_script_ts.R
@@ -3,6 +3,116 @@
 
 # in-class ----------------------------------------------------------------
 
+pacman::p_load(tidyverse,
+               forecast,
+               lterdatasampler,
+               daymetr,
+               glarma)
+
+url <- "https://raw.githubusercontent.com/aterui/biostats/master/data_raw/data_ts_anormaly.csv"
+(df_ts <- read_csv(url))
+
+## draw a figure
+g_base <- df_ts %>% 
+  ggplot(aes(x = year,
+             y = anormaly)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    y = "Anormaly",
+    x = "Year"
+  )
+
+## regression
+m_ts <- lm(anormaly ~ year,
+           data = df_ts)
+
+summary(m_ts)
+
+## figure
+g_base +
+  geom_abline(intercept = coef(m_ts)[1],
+              slope = coef(m_ts)[2]) +
+  theme_bw()
+
+## random-walk
+y <- NULL  
+y[1] <- 0
+
+for (i in 1:99) {
+  y[i + 1] <- y[i] + rnorm(1, mean = 0, sd = 1)
+}
+
+tibble(y = y,
+       x = 1:length(y)) %>% 
+  ggplot(aes(x = x,
+             y = y)) +
+  geom_point() +
+  geom_line()
+
+## Lake Huron data
+df_huron <- tibble(
+  year = time(LakeHuron),                # Extracts the time component (years) from the LakeHuron ts object
+  water_level = as.numeric(LakeHuron)    # Converts LakeHuron values to numeric (from ts class)
+) %>% 
+  arrange(year)  
+
+# Plot Lake Huron time series with a linear trend
+df_huron %>% 
+  ggplot(aes(x = year, y = water_level)) +
+  geom_point(alpha = 0.25) +       # Semi-transparent points
+  geom_line(linetype = "dotted") + # Dotted line connecting points
+  geom_smooth(method = "lm",       # Linear trend line
+              color = "black",
+              linewidth = 0.5) +
+  theme_bw() +
+  labs(x = "Year", y = "Water Level")
+
+## Autoregressive model
+m_ar1 <- Arima(
+  df_huron$water_level,
+  order = c(1, 0, 0)
+)
+
+## fitted values
+df_huron_ar1 <- df_huron %>% 
+  mutate(fit = fitted(m_ar1) %>% 
+           as.numeric())
+
+df_huron_ar1 %>% 
+  ggplot() +
+  geom_point(aes(x = year,
+                 y = water_level),
+             alpha = 0.25) +
+  geom_line(aes(x = year,
+                y = fit),
+            color = "steelblue") +
+  theme_bw()
+
+## Moving-average model
+m_ma1 <- Arima(
+  df_huron$water_level,
+  order = c(0, 0, 1)
+)
+
+## ARMA model
+m_arma1 <- Arima(
+  df_huron$water_level,
+  order = c(1, 0, 1)
+)
+
+## ARIMA model
+m_arima1 <- Arima(
+  df_huron$water_level,
+  order = c(1, 1, 0),
+)
+
+## model selection
+auto.arima(
+  y = df_huron$water_level,
+  stepwise = FALSE,
+  ic = "aic"
+)
 
 # lab ---------------------------------------------------------------------
 
